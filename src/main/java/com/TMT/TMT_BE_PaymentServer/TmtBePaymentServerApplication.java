50f23971573ad515c70d2a3f3b996cfdb5a165c5
[hoontaepark] <pthomj13@gmail.com>
2024-06-13 00:34:01 +0900

    Fix:yml,Gradle 수정

--- src/main/java/com/TMT/TMT_BE_PaymentServer/TmtBePaymentServerApplication.java
+++ src/main/java/com/TMT/TMT_BE_PaymentServer/TmtBePaymentServerApplication.java
@@ -2,12 +2,12 @@ package com.TMT.TMT_BE_PaymentServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class TmtBePaymentServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TmtBePaymentServerApplication.class, args);
