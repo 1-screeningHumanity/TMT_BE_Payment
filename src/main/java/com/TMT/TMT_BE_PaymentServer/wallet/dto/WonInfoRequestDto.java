a62793b85af61442fbab0ff57fb923ce7066275e
[hoontaepark] <pthomj13@gmail.com>
2024-06-14 15:46:45 +0900

    :Hotfi:Won정보 불러오기

---
+++ src/main/java/com/TMT/TMT_BE_PaymentServer/wallet/dto/WonInfoRequestDto.java
@@ -0,0 +1,19 @@
+package com.TMT.TMT_BE_PaymentServer.wallet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WonInfoRequestDto {
+
+    private Long won;
+
+
+    public void getwon(Long won){
+        this.won = won;
+    }
+
+}
